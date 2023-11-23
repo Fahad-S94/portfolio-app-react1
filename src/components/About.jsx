8d61e1d8a8256fa5e40811cbb966b4aa46663a72
Fahad Shoaib <f.shoaib1580@gmail.com>
2023-11-23 02:50:32 -0500

    Updated About.jsx with personal info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,14 +14,10 @@ const About = () => {
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi. I'm Sai Kumar, nice to meet you. Please take a look around.</p>
+              <p>Hi. I'm Fahad Shoaib, nice to meet you. Please take a look around.</p>
             </div>
             <div>
-              <p>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p>  
+              <p><p>I'm a dedicated front-end developer, excited about making my mark in the digital world. As I embark on this journey, I bring a blend of enthusiasm and a genuine eagerness to learn. Crafting user-friendly interfaces and navigating the ever-evolving world of web technologies is where I find my passion. Every project is an opportunity for me to grow and contribute to the dynamic field of front-end development. I'm here to bring a fresh perspective, dedication, and a hunger for knowledge to create engaging online experiences. Let's build something great together!</p>  </p>  
             </div>
           </div>
       </div>
